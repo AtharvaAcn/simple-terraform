220887bce74aa42baba53974dd692fec45b3ea27
AtharvaAcn <atharva.a.deshpande@accenture.com>
2025-05-23 19:37:35 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name = "tfstate"
+    storage_account_name = "tfstate929"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
