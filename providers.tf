99da116288d67d6ea27e5a6baafaad02ff0a83b5
AtharvaAcn <atharva.a.deshpande@accenture.com>
2025-05-27 12:36:20 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name = "tfstate"
-    storage_account_name = "tfstate929"
+    storage_account_name = "tfstate930"
     container_name = "tfstate"
     key = "terraform.tfstate"
   }
